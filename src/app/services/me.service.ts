0ff4258af791d372555f0880cbfdf64abaad7e83
Francisco del Collado <fedelcollado@gmail.com>
2024-01-29 13:31:21 -0600

    conectando los boards del usuario

--- src/app/services/me.service.ts
+++ src/app/services/me.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { User } from '@models/user.model';
 import { checkToken } from '@interceptors/token.interceptor';
+import { Board } from '@models/board.model';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +18,11 @@ export class MeService {
     return this.http.get<User>(`${this.apiUrl}/api/v1/me/profile`, {
       context: checkToken(),
     });
+  }
+
+  getMyBoars() {
+    return this.http.get<Board[]>(`${this.apiUrl}/api/v1/me/boards`, {
+      context: checkToken(),
+    })
   }
 }
